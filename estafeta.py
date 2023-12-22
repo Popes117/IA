75bb7712091ad1c75a65558da5adff178eed8f1f
José Eduardo <103583698+Wally1906@users.noreply.github.com>
2023-12-22 23:24:32 +0000

    Transporte e move adicionado.py

--- estafeta.py
+++ estafeta.py
@@ -1,11 +1,12 @@
 class Estafeta:
     
     #acho que nao precisa ter aqui localização
-    def __init__(self, id, nome, avaliacao, localizacao):
+    def __init__(self, id, nome, avaliacao, localizacao, transport):
         self.id = id
         self.nome = nome
         self.avaliacao = avaliacao
         self.localizacao = localizacao
+        self.transport = transport
 
     def changeLocal(self, newLocal: str):
         self.localizacao = newLocal
@@ -19,3 +20,6 @@ class Estafeta:
 
     def getAvaliacao(self):
         return self.avaliacao
+
+    def move(self, newLocal: str):
+        self.localizacao = newLocal
