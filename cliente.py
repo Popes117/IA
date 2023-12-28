67c016232e8b67d9c5d4c590d2f7d2716eb36f52
José Eduardo <103583698+Wally1906@users.noreply.github.com>
2023-12-28 06:28:29 +0000

    Added getters.py

--- cliente.py
+++ cliente.py
@@ -1,6 +1,12 @@
 class Cliente:
     
-    def __init__(self,id, nome, morada):
+    def __init__(self, nome, morada, id):
         self.id = id
         self.nome = nome
         self.morada = morada
+    
+    def getId(self):
+        return self.id
+    
+    def getRua(self):
+        return self.morada
