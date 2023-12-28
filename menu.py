5274a4f99033ee009c394a38df2210fa6c3ef104
José Eduardo <103583698+Wally1906@users.noreply.github.com>
2023-12-28 06:31:00 +0000

    Added menu

---
+++ menu.py
@@ -0,0 +1,12 @@
+import rua as Rua
+
+class Menu:
+    def printMenu(self):
+        print("[1] Adicionar Cliente")
+        print("[2] Adicionar Estafeta")
+        print("[3] Ver Lista estafetas")
+        print("[4] Ver Lista Clientes")
+        print("[5] Encomendar")
+        print("[6] Repetir Encomenda")
+        print("[0] Sair")
+
