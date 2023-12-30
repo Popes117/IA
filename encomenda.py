4901406741f3df2a2b6bbf8d3014ab26955a7143
José Eduardo <103583698+Wally1906@users.noreply.github.com>
2023-12-30 10:00:53 +0000

    Id Estafeta adicionado.py

--- encomenda.py
+++ encomenda.py
@@ -1,7 +1,8 @@
 class Encomenda:
-    def __init__(self, id, id_cliente, peso, volume, prazo_limite, morada_entrega):
+    def __init__(self, id, id_cliente, id_estafeta, peso, volume, prazo_limite, morada_entrega):
         self.id = id
         self.id_cliente = id_cliente
+        self.id_estafeta = id_estafeta
         self.peso = peso
         self.volume = volume
         self.prazo_limite = prazo_limite
